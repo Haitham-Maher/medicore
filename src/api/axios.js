6628bc20a0afa90d0381f8008c04b3e2ceaf31d0
Haitham <halnmlh@smail.ucas.edu.ps>
2026-05-07 12:27:11 +0300

    feat: link front-end to production back-end server

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://medical-system-master.test/api',
+    baseURL: 'https://medical-system-zw2b.onrender.com',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
